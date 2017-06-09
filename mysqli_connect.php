32fa467b2e5d98fd36f6aa1e05e8ad0d414104fa
ypegasi <ypegasi@hotmail.com>
2017-06-09 12:30:07 +0200

    Initial commit

---
+++ mysqli_connect.php
@@ -0,0 +1,15 @@
+<?php
+//This file provides the information for accessing the database and connecting
+//to MySQL. It alsosets the language coding to utf-8
+//First we define the constant:
+
+DEFINE  ('DB_USER', 'horatio');
+DEFINE  ('DB_PASSWORD', 'hmsvictory');
+DEFINE  ('DB_HOST', 'localhost');
+DEFINE  ('DB_DBNAME', 'simpleIdb');
+// Next we assign the database connection to a variable that we will call$dbcon:
+$dbcon =@mysqli_connect(DB_HOST, DB_NAME, DB_PASSWORD, DB_NAME)
+OR die ('Could not connect to MySQL: ' . mysql_connect_error() );
+//Finally, we set the language encoding as uf-8
+mysqli_set_charset($dbcon, 'utf8');     
+?>
---
+++ register-thanks.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang=en>
+<head>
+<title>Registration thank you page</title>
+<meta charset=utf-8>
+<link rel="stylesheet" type="text/css" href="includes.css">
+</head>
+<body>
+<div id="container">
+<?php include("header.php"); ?>
+<?php include("nav.php"); ?>
+<?php include("info-col.php"); ?>
+<div id="content"><!-- Start of the thank you page-content. -->
+<h2>Thank you for registering</h2>
+<p>The thank you page content. The thank you page content. The thank you page content. 
+The thank you page content. The thank you page content. The thank you page content. 
+The thank you page content. The thank you page content. The thank you page content. 
+The thank you page content. The thank you page content. The thank you page content. 
+The thank you page content. The thank you page content. The thank you page content.</p>
+<!-- End of the thank you page-content. -->
+</div>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
